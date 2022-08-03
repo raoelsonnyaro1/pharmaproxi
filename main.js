4e836d7338f715f7f292af46a5adabfc79f0e090
raoelsonnyaro1 <raoelsonnyaro@gmail.com>
2022-08-03 13:12:50 +0300

    img

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 $(document).ready(function () {
-    $('.article-carousel').owlCarousel({
+  $(".article-carousel").owlCarousel({
+    items: 3,
+    loop: true,
+    margin: 30,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      480: {
+        items: 1,
+      },
+      700: {
+        items: 2,
+      },
+      769: {
         items: 3,
-        loop: true,
-        margin: 30,
-        dots: true,
-    });
+      },
+    },
+  });
 });
